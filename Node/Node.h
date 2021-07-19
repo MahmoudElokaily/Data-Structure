35e6ad75197c973fb5418eafd7e48e50664e5960
Mahmoud Elokaily <70199596+MahmoudElokaily@users.noreply.github.com>
2021-07-19 05:16:36 +0200

    Add file Node.h

---
+++ Node/Node.h
@@ -0,0 +1,22 @@
+//
+// Created by M Elokaily on 7/17/2021.
+//
+
+#ifndef NODE_NODE_H
+#define NODE_NODE_H
+
+template <class t>
+class Node {
+private:
+    t value;
+    Node * nxt;
+public:
+    Node();
+    Node(t);
+    void setValue(t);
+    t& getValue() const;
+    void setNxt(Node<t>*);
+    Node<t>* getNxt() const;
+};
+
+#endif //NODE_NODE_H
