758f5488c44a459248e52023b61ba3880a2e69ea
Mahmoud Elokaily <70199596+MahmoudElokaily@users.noreply.github.com>
2021-07-19 05:30:29 +0200

    Add string.h

---
+++ String/string.h
@@ -0,0 +1,28 @@
+//
+// Created by M Elokaily on 7/17/2021.
+//
+
+#ifndef MAIN_CPP_STRING_H
+#define MAIN_CPP_STRING_H
+
+
+class string {
+private:
+    int length,max;
+    char str[];
+public:
+    string();
+    void push_back(char);
+    void pop_back();
+    bool isEmpty() const;
+    int size()const;
+    void swapString();
+    void swap(int,int);
+    char back() const;
+    char front() const;
+    void substr(int,int);
+    void Display();
+};
+
+
+#endif //MAIN_CPP_STRING_H
