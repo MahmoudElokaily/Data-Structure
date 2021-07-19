6d86aca1e153fc61988f151fc4ae3af7ce85434c
Mahmoud Elokaily <70199596+MahmoudElokaily@users.noreply.github.com>
2021-07-19 05:22:47 +0200

    Add stack.h

---
+++ Stack/Stack.h
@@ -0,0 +1,32 @@
+//
+// Created by M Elokaily on 7/19/2021.
+//
+
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+#include "D:/Data Structure/Node/Node.h"
+
+
+#include "../Node/Node.h"
+#include <iostream>
+
+using namespace std;
+
+template <class t>
+class Stack{
+private:
+    int length;
+    Node<t>* top;
+public:
+    Stack();
+    void push(t);
+    void pop();
+    void clear();
+    void Display();
+    int find(t);
+    t& getTop() const;
+    int size() const;
+    bool isEmpty() const;
+};
+
+#endif //STACK_STACK_H
