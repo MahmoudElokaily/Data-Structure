847ea3e3997ac88df2eecb9f34db594baf0680bf
Mahmoud Elokaily <70199596+MahmoudElokaily@users.noreply.github.com>
2021-07-19 05:28:01 +0200

    Add PQ.h

---
+++ Priority Queue/PriorityQueue.h
@@ -0,0 +1,27 @@
+//
+// Created by M Elokaily on 7/17/2021.
+//
+
+#ifndef MAIN_CPP_PRIORITYQUEUE_H
+#define MAIN_CPP_PRIORITYQUEUE_H
+#include <iostream>
+using namespace std;
+
+template <class t>
+class PriorityQueue {
+private:
+    int length;
+    int max;
+    t PQ[];
+public:
+    PriorityQueue();
+    bool isEmpty() const;
+    void insert(t);
+    void remove();
+    void Display();
+    int size() const;
+    t top() const;
+    void clear();
+};
+
+#endif //MAIN_CPP_PRIORITYQUEUE_H
